f6ccf2c822532a4bbf943323e061cf41faf12c22
zamzani <lhh042615@gmail.com>
2023-05-17 14:47:41 +0900

    5/17_14.47

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from polls.models import Question, Choice
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub date', 'question_text']
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
 
